2d440824a024bdf2c5465946e172f28f805a121c
Sebastian <sebastianmeyer0000@gmail.com>
2021-02-23 11:50:23 -0300

    cronjob hours

--- cronjob.py
+++ cronjob.py
@@ -6,6 +6,6 @@ from listener import twitter_listener
 
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
-scheduler.add_job(twitter_listener, "interval", hour=1)
+scheduler.add_job(twitter_listener, "interval", hours=1)
 
 scheduler.start()
